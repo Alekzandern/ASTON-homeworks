c7bb3e3ddb2453f134e8c0d50eb6762e49b280e9
Alexzander <Alex.Zander54.ru>
2024-10-06 13:25:12 +0700

    выполнение задания 10

---
+++ src/paradigma_10/OnePerson.java
@@ -0,0 +1,41 @@
+package paradigma_10;
+
+public class OnePerson {
+    public static void main(String[] args) {
+        class Сотрудник {
+
+            public String ФИО;
+            public String должность;
+            public String емэйл;
+            public String телефон;
+            public int зарплата;
+            public int возраст;
+
+            public Сотрудник(String ФИО, String должность, String емэйл, String телефон, int зарплата, int возраст) {
+                this.ФИО = ФИО;
+                this.должность = должность;
+                this.емэйл = емэйл;
+                this.телефон = телефон;
+                this.зарплата = зарплата;
+                this.возраст = возраст;
+            }
+        }
+
+        class EmployeePrinter {
+            public void printEmployeeDetails(Сотрудник сотрудник) {
+                System.out.println("ФИО: " + сотрудник.ФИО);
+                System.out.println("Должность: " + сотрудник.должность);
+                System.out.println("Email: " + сотрудник.емэйл);
+                System.out.println("Телефон: " + сотрудник.телефон);
+                System.out.println("Зарплата: " + сотрудник.зарплата);
+                System.out.println("Возраст: " + сотрудник.возраст);
+            }
+        }
+
+        Сотрудник сотрудник1 = new Сотрудник("Иванов Иван Иванович", "Тестер",
+                "ivanov@.com", "+79123456789", 900000, 40);
+
+        EmployeePrinter printer = new EmployeePrinter();
+        printer.printEmployeeDetails(сотрудник1);
+    }
+}
