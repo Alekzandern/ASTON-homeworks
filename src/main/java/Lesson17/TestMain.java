6931a4f5cc857cf4ce80d60d03c67bb28192bc9c
Alexzander <Alex.Zander54.ru>
2024-11-01 21:26:09 +0700

    выполнение задания 17

--- src/main/java/Lesson17/TestMain.java
+++ src/main/java/Lesson17/TestMain.java
@@ -1,5 +1,6 @@
 package Lesson17;
 
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -12,9 +13,47 @@ public class TestMain {
     }
 
     @Test
+    public void WhenGetRequestWithTextBody_statusCode() {
+        given().baseUri("https://postman-echo.com").contentType(ContentType.TEXT).body("text").when().get("/get").then().assertThat().statusCode(200);
+    }
+
+    @Test
     public void WhenDeleteRequest_status() {
         given().baseUri("https://postman-echo.com").when().delete("/delete").then()
                 .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void WhenPostRequestWithTextBody_statusCode() {
+        given().baseUri("https://postman-echo.com").contentType(ContentType.TEXT).body("text").when().post("/post").then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void WhenPostRequestWithTextBody_body() {
+        given().baseUri("https://postman-echo.com").contentType(ContentType.TEXT).body("text").when().post("/post").then().log().body().and().body("data", equalTo("text"));
+    }
+
+    @Test
+    public void WhenPostRequestWithFormData_statusCode() {
+        given().baseUri("https://postman-echo.com").contentType(ContentType.URLENC).formParam("key", "value").when().post("/post").then().assertThat().statusCode(500);
+    }
+
+    @Test
+    public void WhenPostRequestWithFormData_body() {
+        given().baseUri("https://postman-echo.com").contentType(ContentType.URLENC).formParam("key", "value").when().post("/post").then().log().body().and().body("data", equalTo("key=value"));
+    }
+
+    @Test
+    public void WhenPutRequest_statusCode() {
+        given().baseUri("https://postman-echo.com").when().put("/put").then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void WhenPutRequest_body() {
+        given().baseUri("https://postman-echo.com")
+                .contentType(ContentType.JSON)
+                .body("{\"key\": \"value\"}")
+                .when().put("/put").then().log().body().and().body("key", equalTo("value"));
     }
 
     @Test
